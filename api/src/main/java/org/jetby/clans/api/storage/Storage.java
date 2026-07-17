daf564c5e49c02d8c7b2c7cdd3543a2c5cb0c41b
MrJetby <aminfazlonshoev@gmail.com>
2026-07-16 23:37:35 -0400

    remove Lifecycle, Lookup extends

--- api/src/main/java/org/jetby/clans/api/storage/Storage.java
+++ api/src/main/java/org/jetby/clans/api/storage/Storage.java
@@ -1,13 +1,11 @@
 package org.jetby.clans.api.storage;
 
 import org.jetby.clans.api.service.clan.Clan;
-import org.jetby.clans.api.service.clan.Lifecycle;
-import org.jetby.clans.api.service.clan.Lookup;
 import org.jetby.clans.api.storage.base.BaseSection;
 
 import java.util.List;
 
-public interface Storage extends Lifecycle, Lookup {
+public interface Storage {
 
 
     /* Get the plugin section */
