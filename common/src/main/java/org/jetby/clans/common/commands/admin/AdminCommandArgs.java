f508e1ef6573c3b208d10ee1cac5742f38d4f6f8
MrJetby <aminfazlonshoev@gmail.com>
2026-07-17 19:26:51 -0400

    add enum `CLAN` remove enum `STORAGE` because clan already contains that

--- common/src/main/java/org/jetby/clans/common/commands/admin/AdminCommandArgs.java
+++ common/src/main/java/org/jetby/clans/common/commands/admin/AdminCommandArgs.java
@@ -3,15 +3,12 @@ package org.jetby.clans.common.commands.admin;
 import lombok.Getter;
 import org.jetby.clans.api.command.Subcommand;
 import org.jetby.clans.common.TreexClans;
-import org.jetby.clans.common.commands.admin.subcommands.CoinSubcommand;
-import org.jetby.clans.common.commands.admin.subcommands.ExpSubcommand;
-import org.jetby.clans.common.commands.admin.subcommands.ReloadSubcommand;
-import org.jetby.clans.common.commands.admin.subcommands.StorageSubcommand;
+import org.jetby.clans.common.commands.admin.subcommands.*;
 
 public enum AdminCommandArgs {
     COIN(new CoinSubcommand()),
     EXP(new ExpSubcommand()),
-    STORAGE(new StorageSubcommand()),
+    CLAN(new ClanSubcommand()),
     RELOAD(new ReloadSubcommand(TreexClans.getInstance()));
 
     @Getter
